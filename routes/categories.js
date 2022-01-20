06736d4ff359b63d424644b8e58023152907c746
eneskocadag <eneskocadag5e@gmail.com>
2022-01-20 16:38:00 +0300

    Added single category get endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -2,5 +2,6 @@ const express = require("express");
 const routes = express.Router();
 const categoriesController = require("../controllers/Categories");
 
-routes.get("/",categoriesController.multigetCategories);
+routes.get("/", categoriesController.multigetCategories);
+routes.get("/:categoryName", categoriesController.singlegetCategories);
 module.exports = routes;
