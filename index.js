1d09ce5e1394c0997a06fa12aad1b3dab09f16e9
mfurkanayhan <mfurkana067@gmail.com>
2022-01-21 15:47:02 +0300

    Added Error Handler and Project is ended

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express();
 const api = require("./routes/api");
 //! Helpers
 const jwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 app.use(express.json());
 //! Api Routes & jwt
 app.use(jwt());
 app.use("/api", api);
+app.use(errorHandler)
 
 //! Server Listening
 app.listen(3000, () => {
